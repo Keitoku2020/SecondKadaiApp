bc3437ceaef2375ee2c1faa54645c3890e9eb510
keitoku.ikegami <keitoku.ikegami2020@klas.ch>
2020-02-14 07:35:54 +0100

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by IKEGAMIKeitoku on 14.02.20.
+//  Copyright © 2020 keitoku.ikegami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
